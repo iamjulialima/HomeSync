a5f6e08ff9497ee991c2a52a3fe66aeb7194f270
MarcelaDomici <marceladomiciano2@gmail.com>
2025-06-04 21:53:23 -0300

    feat: sql tabelas luzes

--- database/createTables.js
+++ database/createTables.js
@@ -14,3 +14,34 @@ db.run(`CREATE TABLE IF NOT EXISTS usuario (
   }
 });
 
+// Criação da tabela estados da luz
+db.run(`CREATE TABLE IF NOT EXISTS luzesStatus(
+  cod_luz  INTEGER PRIMARY KEY,
+  estado TEXT NOT NULL,
+  nome TEXT NOT NULL,
+  localizacao TEXT NOT NULL,
+  intensidade TEXT NOT NULL
+  )`, (err) => {
+  if (err) {
+    console.error('Erro ao criar tabela luzesStatus:', err.message);
+  } else {
+    console.log('Tabela luzesStatus verificada/criada com sucesso.');
+  }
+});
+
+// Criação tabela de agendamentos de luz
+db.run(`CREATE TABLE IF NOT EXISTS agendamentoLuzes(
+  cod_agendamento INTEGER PRIMARY KEY AUTOINCREMENT,
+  acao TEXT NOT NULL,
+  luzes TEXT  NOT NULL,
+  data TEXT NOT NULL,
+  hora TEXT NOT NULL,
+  repetir TEXT NOT NULL,
+  intensidade TEXT NOT NULL
+  )`, (err) => {
+  if (err) {
+    console.error('Erro ao criar tabela luzesStatus:', err.message);
+  } else {
+    console.log('Tabela luzesStatus verificada/criada com sucesso.');
+  }
+});
